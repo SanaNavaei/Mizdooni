ae0416938dfcc054569cd55a857e1535b5916208
SanaNavaei <sana.sarinavaei@gmail.com>
2024-04-28 17:14:50 +0330

    chore: add base react app

---
+++ CA4/front-end/src/pages/App.js
@@ -0,0 +1,15 @@
+import '../assets/stylesheets/App.css';
+import logo from '../assets/images/logo.png'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Reserve restaurant tables!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
