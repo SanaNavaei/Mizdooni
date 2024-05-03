c0278bbb4d93e5fdcd7b050bd39dc23476822f8d
SanaNavaei <sana.sarinavaei@gmail.com>
2024-05-03 13:11:13 +0330

    feat: add CompleteReserveModal

--- CA4/front-end/src/components/Reserve.js
+++ CA4/front-end/src/components/Reserve.js
@@ -1,26 +1,68 @@
 import { useState, useEffect } from 'react';
 
-function Reserve({ maxSeatNumber, availableTimes }) {
+import CompleteReserveModal from './CompleteReserveModal';
+
+const getCurrentDate = () => {
+  const date = new Date();
+  const year = date.getFullYear();
+  let month = date.getMonth() + 1;
+  let day = date.getDate();
+
+  month = month < 10 ? '0' + month : month;
+  day = day < 10 ? '0' + day : day;
+
+  return `${year}-${month}-${day}`;
+};
+
+function Reserve({ maxSeatNumber, availableTimes, country, city, street }) {
   const [selectedDate, setSelectedDate] = useState('');
+  const [selectedTime, setSelectedTime] = useState('');
+  const [selectedSeat, setSelectedSeat] = useState(0);
   const [filteredAvailableTimes, setFilteredAvailableTimes] = useState([]);
+  const [isButtonDisabled, setIsButtonDisabled] = useState(true);
+  const [errorMessage, setErrorMessage] = useState('');
 
   useEffect(() => {
-    setFilteredAvailableTimes(availableTimes);
-  }, [availableTimes]);
+    if (selectedDate) {
+      const filteredTimes = availableTimes.filter(time => time.date === selectedDate);
+      setFilteredAvailableTimes(filteredTimes);
+      setErrorMessage('');
+    } else {
+      setFilteredAvailableTimes([]);
+    }
+  }, [selectedDate, availableTimes]);
 
-  const seatOptions = [];
+  useEffect(() => {
+    setIsButtonDisabled(!(selectedDate && selectedTime && selectedSeat));
+  }, [selectedDate, selectedTime, selectedSeat]);
+
+  const seatOptions = [<option key="empty" value=""></option>];
   for (let i = 1; i <= maxSeatNumber; i++) {
     seatOptions.push(<option key={i} value={i}>{i}</option>);
   }
 
   const handleDateChange = (e) => {
-    this.selectedDate = e.target.value;
-    setSelectedDate(selectedDate);
+    const selected = new Date(e.target.value);
+    const today = new Date();
+    const oneMonthFromNow = new Date(today.getFullYear(), today.getMonth() + 1, today.getDate());
 
-    const filteredTimes = availableTimes.filter(time => {
-      return time.date === selectedDate;
-    });
-    setFilteredAvailableTimes(filteredTimes);
+    if (selected > oneMonthFromNow) {
+      const maxDate = `${oneMonthFromNow.getFullYear()}-${('0' + (oneMonthFromNow.getMonth() + 1)).slice(-2)}-${('0' + oneMonthFromNow.getDate()).slice(-2)}`;
+      setErrorMessage(`The maximum possible date for reservation is ${maxDate}`);
+      setSelectedDate('');
+    } else {
+      setSelectedDate(e.target.value);
+    }
+  };
+
+  const handleTimeChange = (e) => {
+    setSelectedTime(e.target.value);
+  };
+
+  const handleSeatChange = (e) => {
+    if (e !== "") {
+      setSelectedSeat(e.target.value);
+    }
   };
 
   return (
@@ -29,30 +71,52 @@ function Reserve({ maxSeatNumber, availableTimes }) {
       <form id="reserve-form">
         <div className="ps-2 ps-sm-0">
           <label htmlFor="people">For</label>
-          <select className="form-select mx-1" name="people" id="people">{seatOptions}</select>
+          <select className="form-select mx-1" name="people" id="people" onChange={handleSeatChange} value={selectedSeat}>{seatOptions}</select>
           <span>people, </span>
           <br className="d-sm-none" />
           <label className="pt-3 pt-sm-0" htmlFor="date"> on date </label>
-          <input className="form-control mx-1" type="date" name="date" id="date" required onChange={handleDateChange} />
+          <input className="form-control mx-1" type="date" name="date" id="date" min={getCurrentDate()} required onChange={handleDateChange} />
         </div>
 
-        <p className="mt-3 mb-2">Available Times</p>
-        <div className="row row-cols-4 row-cols-md-5 row-cols-lg-4 g-2">
-          {filteredAvailableTimes.map((time, index) => (
-            <div key={index} className="col">
-              <input className="btn-check" type="radio" name="time" value={time.time} id={`time${index + 1}`} required />
-              <label className="available-time d-block btn" htmlFor={`time${index + 1}`}>{time.time}</label>
-            </div>
-          ))}
-        </div>
+        {errorMessage && (
+          <p className="miz-text-red my-4">{errorMessage}</p>
+        )}
 
-        <p id="one-hour-text" className="miz-text-red my-2">
-          You will reserve this table only for
-          <span className="text-decoration-underline"> one </span>
-          hour, for more time please contact the restaurant.
-        </p>
-        <button type="submit" className="miz-button w-100">Complete the Reservation</button>
+        {!errorMessage && selectedDate && (
+          <>
+            {filteredAvailableTimes.length > 0 ? (
+              <>
+                <p className="mt-3 mb-2">Available Times</p>ّ
+                <div className="row row-cols-4 row-cols-md-5 row-cols-lg-4 g-2">
+                  {filteredAvailableTimes.map((time, index) => (
+                    <div key={index} className="col">
+                      <input className="btn-check" type="radio" name="time" value={time.time} id={`time${index + 1}`} required onChange={handleTimeChange} />
+                      <label className="available-time d-block btn" htmlFor={`time${index + 1}`}>{time.time}</label>
+                    </div>
+                  ))}
+                </div>
+                <p id="one-hour-text" className="miz-text-red my-2">
+                  You will reserve this table only for
+                  <span className="text-decoration-underline"> one </span>
+                  hour, for more time please contact the restaurant.
+                </p>
+              </>
+            ) : (
+              <p className="my-4">No Table is available on this date.</p>
+            )}
+          </>
+        )}
+
+        {!errorMessage && !selectedDate && (
+          <div>
+            <p className="mt-3 mb-2">Available Times</p>
+            <p className="miz-text-red my-2">Select the number of people and date.</p>
+          </div>
+        )}
+        <button type="submit" className="miz-button disabled-button w-100" data-bs-toggle="modal" data-bs-target="#completeReservation" disabled={isButtonDisabled}>Complete the Reservation</button>
       </form>
+
+      <CompleteReserveModal country={country} city={city} street={street} tableNumber={selectedSeat} time={selectedTime} />
     </section>
   );
 }
