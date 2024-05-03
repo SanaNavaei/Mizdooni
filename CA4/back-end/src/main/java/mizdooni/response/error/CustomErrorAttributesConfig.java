ea5d57eadcf35823dbf1e8a97523a095470b2f71
MisaghM <misagh.mhgg@gmail.com>
2024-05-03 19:52:07 +0330

    feat: add custom error controller

---
+++ CA4/back-end/src/main/java/mizdooni/response/error/CustomErrorAttributesConfig.java
@@ -0,0 +1,13 @@
+package mizdooni.response.error;
+
+import org.springframework.boot.web.servlet.error.ErrorAttributes;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+class CustomErrorAttributesConfig {
+    @Bean
+    public ErrorAttributes errorAttributes() {
+        return new CustomErrorAttributes();
+    }
+}
