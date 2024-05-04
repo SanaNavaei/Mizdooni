bbc934778a6fda139a322fd091bf3fa9bf9ef597
SanaNavaei <sana.sarinavaei@gmail.com>
2024-05-04 10:46:48 +0330

    feat: request and response handling in home page

--- CA4/front-end/src/pages/Home.js
+++ CA4/front-end/src/pages/Home.js
@@ -1,149 +1,43 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
 import PageLayout from 'components/PageLayout';
 import HomeSearch from 'components/HomeSearch';
 import Cards from 'components/Cards';
 import About from 'components/About';
 
-import TheIvyBrasserie from 'assets/images/restaurants/The-Ivy-Brasserie-1.png';
-import HaliaRestaurant from 'assets/images/restaurants/Halia-Restaurant.png';
-import ElineCafe from 'assets/images/restaurants/Eline-Cafe.png';
-import Cecconis from 'assets/images/restaurants/Cecconis.png';
-import HardRockCafe from 'assets/images/restaurants/Hard-Rock-Cafe.png';
-import TwentyEightFiftyMarylebone from 'assets/images/restaurants/28-50-Marylebone.png';
-
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'assets/stylesheets/global.css';
 import 'assets/stylesheets/cards.css';
 import 'assets/stylesheets/home.css';
 
-const restaurants = [
-  {
-    name: 'The Ivy Brasserie',
-    reviews: 2096,
-    type: 'Contemporary British',
-    location: 'Tehran',
-    isOpen: true,
-    closingTime: '11 PM',
-    starCount: 5,
-    image: TheIvyBrasserie
-  },
-  {
-    name: 'Halia Restaurant',
-    reviews: 12096,
-    type: 'Fast Food',
-    location: 'Karaj',
-    isOpen: false,
-    startingTime: '10 AM',
-    starCount: 5,
-    image: HaliaRestaurant
-  },
-  {
-    name: 'Eline Cafe',
-    reviews: 0,
-    type: 'Sea Food',
-    location: 'Rasht',
-    isOpen: true,
-    closingTime: '12 PM',
-    starCount: 5,
-    image: ElineCafe
-  },
-  {
-    name: 'Cecconi’s',
-    reviews: 2096,
-    type: 'Healthy Food',
-    location: 'Ahvaz',
-    isOpen: true,
-    closingTime: '11 PM',
-    starCount: 5,
-    image: Cecconis
-  },
-  {
-    name: 'Hard Rock Cafe',
-    reviews: 2096,
-    type: 'Vegetarian Food',
-    location: 'Tabriz',
-    isOpen: true,
-    closingTime: '11 PM',
-    starCount: 5,
-    image: HardRockCafe
-  },
-  {
-    name: '28 - 50 Marylebone',
-    reviews: 2096,
-    type: 'Kebab',
-    location: 'Tehran',
-    isOpen: true,
-    closingTime: '11 PM',
-    starCount: 5,
-    image: TwentyEightFiftyMarylebone
-  },
-  {
-    name: 'The Ivy Brasserie',
-    reviews: 2096,
-    type: 'Contemporary British',
-    location: 'Tehran',
-    isOpen: true,
-    closingTime: '11 PM',
-    starCount: 5,
-    image: TheIvyBrasserie
-  },
-  {
-    name: 'Halia Restaurant',
-    reviews: 12096,
-    type: 'Fast Food',
-    location: 'Karaj',
-    isOpen: false,
-    startingTime: '10 AM',
-    starCount: 5,
-    image: HaliaRestaurant
-  },
-  {
-    name: 'Eline Cafe',
-    reviews: 0,
-    type: 'Sea Food',
-    location: 'Rasht',
-    isOpen: true,
-    closingTime: '12 PM',
-    starCount: 5,
-    image: ElineCafe
-  },
-  {
-    name: 'Cecconi’s',
-    reviews: 2096,
-    type: 'Healthy Food',
-    location: 'Ahvaz',
-    isOpen: true,
-    closingTime: '11 PM',
-    starCount: 5,
-    image: Cecconis
-  },
-  {
-    name: 'Hard Rock Cafe',
-    reviews: 2096,
-    type: 'Vegetarian Food',
-    location: 'Tabriz',
-    isOpen: true,
-    closingTime: '11 PM',
-    starCount: 5,
-    image: HardRockCafe
-  },
-  {
-    name: '28 - 50 Marylebone',
-    reviews: 2096,
-    type: 'Kebab',
-    location: 'Tehran',
-    isOpen: true,
-    closingTime: '11 PM',
-    starCount: 5,
-    image: TwentyEightFiftyMarylebone
-  }
-];
-
 function Home() {
+  const [restaurants, setRestaurants] = useState([]);
+
   useEffect(() => {
     document.title = 'Home';
   }, []);
+
+  useEffect(() => {
+    fetch('/api/restaurants/top', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Failed to fetch restaurants');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log('Success:', data);
+        setRestaurants(data);
+      })
+      .catch(error => {
+        console.error('Error fetching restaurants:', error);
+      });
+  })
 
   return (
     <PageLayout>
